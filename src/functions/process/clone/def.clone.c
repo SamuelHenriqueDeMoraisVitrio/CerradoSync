3b7ea88c59e7c53e4142e0428115c84408d6fc4b
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-12-24 12:20:18 -0300

    import automatic

--- src/functions/process/clone/def.clone.c
+++ src/functions/process/clone/def.clone.c
@@ -2,7 +2,6 @@
 //silver_chain_scope_start
 //mannaged by silver chain
 #include "../../../imports/imports.dec.h"
-#include <sched.h>
 //silver_chain_scope_end
 
 int private_CerradoSync_processThread_config(void *arg){
